4a9b37b6f6b478a9df0c658b067ccea171b39feb
Leon <leon.shainski@gmail.com>
2021-02-05 23:16:09 -0500

    Testing

--- app/src/main/java/com/example/hackathonapp/MainActivity.java
+++ app/src/main/java/com/example/hackathonapp/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    @Override 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
