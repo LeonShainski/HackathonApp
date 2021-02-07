884b96bd3c4c21efa79256eeb232d5ae9564ab38
Leon <leon.shainski@gmail.com>
2021-02-07 01:47:47 -0500

    Adding a blueprint to add tasks

--- app/src/main/java/com/example/hackathonapp/AddTasks.java
+++ app/src/main/java/com/example/hackathonapp/AddTasks.java
@@ -3,12 +3,22 @@ package com.example.hackathonapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class AddTasks extends AppCompatActivity {
+
+    private Button btnAddTaskSubmit;
+    private RadioButton radioCategory1, radioCategory2, radioCategory3;
+    private TextView txtTaskToAdd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_tasks);
+
+
     }
 }
