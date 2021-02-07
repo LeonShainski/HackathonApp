789dfc13b05becc3d089e79566a8cefe048a5add
Leon <leon.shainski@gmail.com>
2021-02-06 20:36:49 -0500

    temp commit

---
+++ app/src/main/java/com/example/hackathonapp/TestingActivity.java
@@ -0,0 +1,14 @@
+package com.example.hackathonapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testing);
+    }
+}
