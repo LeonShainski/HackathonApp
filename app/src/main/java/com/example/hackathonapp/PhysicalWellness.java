1a118dc7f3e4f55a67c078d5373b62a9096e7ff9
Leon <leon.shainski@gmail.com>
2021-02-07 06:10:08 -0500

    Made some changes

--- app/src/main/java/com/example/hackathonapp/PhysicalWellness.java
+++ app/src/main/java/com/example/hackathonapp/PhysicalWellness.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class PhysicalWellness extends AppCompatActivity {
@@ -19,17 +21,21 @@ public class PhysicalWellness extends AppCompatActivity {
     private CheckBox checkbox2PW;
     private CheckBox checkbox3PW;
     private CheckBox checkbox4PW;
+    private CheckBox checkbox5PW;
+    private TextView textbox5PW;
 
     public static final String SHARED_PREFS_PW = "SharedPrefs_PW";
     public static final String CHECK1_PW = "Checkbox1_PW";
     public static final String CHECK2_PW = "Checkbox2_PW";
     public static final String CHECK3_PW = "Checkbox3_PW";
     public static final String CHECK4_PW = "Checkbox4_PW";
+    public static final String CHECK5_PW = "Checkbox4_PW";
 
     private Boolean check1_PW;
     private Boolean check2_PW;
     private Boolean check3_PW;
     private Boolean check4_PW;
+    private Boolean check5_PW;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +47,12 @@ public class PhysicalWellness extends AppCompatActivity {
         checkbox2PW = (CheckBox) findViewById(R.id.chkPhysical2);
         checkbox3PW = (CheckBox) findViewById(R.id.chkPhysical3);
         checkbox4PW = (CheckBox) findViewById(R.id.chkPhysical4);
+        checkbox5PW = (CheckBox) findViewById(R.id.chkPhysical5);
+        textbox5PW = findViewById(R.id.txtPhysicalBox5);
+
+        checkbox5PW.setVisibility(View.GONE);
+        //checkbox5PW.setText("Some text");
+        textbox5PW.setVisibility(View.GONE);
 
         finishedBoxPW.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,6 +77,7 @@ public class PhysicalWellness extends AppCompatActivity {
         editor.putBoolean(CHECK2_PW, checkbox2PW.isChecked());
         editor.putBoolean(CHECK3_PW, checkbox3PW.isChecked());
         editor.putBoolean(CHECK4_PW, checkbox4PW.isChecked());
+        editor.putBoolean(CHECK5_PW, checkbox4PW.isChecked());
 
         editor.apply();
 
@@ -77,6 +90,7 @@ public class PhysicalWellness extends AppCompatActivity {
         check2_PW = sharedPreferences.getBoolean(CHECK2_PW, false);
         check3_PW = sharedPreferences.getBoolean(CHECK3_PW, false);
         check4_PW = sharedPreferences.getBoolean(CHECK4_PW, false);
+        check5_PW = sharedPreferences.getBoolean(CHECK5_PW, false);
     }
 
     public void updateViews(){
@@ -84,6 +98,7 @@ public class PhysicalWellness extends AppCompatActivity {
         checkbox2PW.setChecked(check2_PW);
         checkbox3PW.setChecked(check3_PW);
         checkbox4PW.setChecked(check4_PW);
+        checkbox4PW.setChecked(check5_PW);
     }
 
     public int loadPercentage(){
@@ -99,6 +114,8 @@ public class PhysicalWellness extends AppCompatActivity {
         }
         if (checkbox4PW.isChecked()){
             physicalPercent++;
+        } if (checkbox5PW.isChecked()){
+            physicalPercent++;
         }
         physicalPercent = physicalPercent * 25;
         return physicalPercent;
@@ -108,5 +125,11 @@ public class PhysicalWellness extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         intent.putExtra("physicalWellnessPercent", physicalPercent);
         startActivity(intent);
+    }
+
+    public void addNew (String name) {
+        checkbox5PW.setVisibility(View.VISIBLE);
+        checkbox5PW.setText("Some text");
+        textbox5PW.setVisibility(View.VISIBLE);
     }
 }
